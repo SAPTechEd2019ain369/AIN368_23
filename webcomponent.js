ad7deaba886497d3b607536c2b0e16e31205d10b
SAPTechEd2019ain369 <52492960+SAPTechEd2019ain369@users.noreply.github.com>
2019-09-25 18:24:11 -0700

    Ex3_023

--- webcomponent.js
+++ webcomponent.js
@@ -61,6 +61,20 @@
             this.redraw();
         };
           
+        //Getters and Setters
+        get angleMax() {
+            return this._endAngleDeg;
+        }
+        set angleMax(value) {
+            //Empty the shadow dom
+            if (this._svgContainer){
+            this._svgContainer._groups[0][0].innerHTML = "";
+            }
+        
+            this._endAngleDeg = value;
+            this.redraw();
+        };
+
         redraw() 
         {
                 if (!this._svgContainer){
